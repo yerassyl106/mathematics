b2826d685948564e304fbdcc207a085734d60681
yerassyl106 <yerassyl106@gmail.com>
2024-10-31 18:06:25 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,99 @@
+// var a = 21;
+// var b = 12;
+
+// console.log(a-b);
+
+// var a = 45;
+// var b = 25;
+
+// var total = a * b;
+
+
+// console.log(total);
+
+
+
+
+
+
+// let a = 10;
+// let b = 5;
+
+
+// console.log(a / b);
+
+// let a = 10;
+// let b = 3;
+
+// console.log(a % b);
+
+
+
+// var a = 200;
+
+// a++;          // a = a + 1;
+
+// a--;
+
+// console.log(a);
+
+
+
+
+// var num = 150 + 60;
+
+// num +=30  //num = num + 30;
+
+// console.log(num);
+
+
+
+
+// let x = 5;
+
+
+// x -=5;  //x= x - 5
+
+// console.log(x);
+
+
+
+
+// let x = 5;
+
+
+// x *=5;  //x= x * 5
+
+// console.log(x);
+
+
+
+
+// let x = 5;
+
+
+// x /=5;  //x= x / 5
+
+// console.log(x);
+
+
+
+
+
+
+// let x = 5;
+
+
+// x %=3   ;  //x= x % 5
+
+// console.log(x);
+
+var a = 100;
+var b = 50
+var c = 40
+var d = 30
+var e = 20 
+
+let total = (a + b)  *  c - d / e;
+
+console.log(total);
